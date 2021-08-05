35a643b8b2b34d6e431c65c58bbacde73b5a7069
Matthias Denu <matthiasdenu@outlook.com>
2021-08-04 22:28:28 -0400

    Update CustomPluginMangerSpecification.groovy

--- src/test/groovy/CustomPluginMangerSpecification.groovy
+++ src/test/groovy/CustomPluginMangerSpecification.groovy
@@ -10,13 +10,15 @@ class CustomPluginMangerSpecification extends BaseSpecification {
     @Override
     PluginManager getPluginManager() {
         // This breaks `gradle test` with the "java.lang.AssertionError: class
-        //   org.jenkinsci.plugins.workflow.job.WorkflowJob is missing its descriptor"
-        //   error -- You can you one instance of one type, but you can't start a new Spock spec
-        //   with a new JenkinsRule of another type or you will get this error -- At the end of the
-        //   day, I want to use the resolveTestPluginsCustom gradle task in build.gradle to test
-        //   some of the Jenkins job DSL with a different set of plugins of different specs... So if
-        //   there is another way to do this, I am all ears... I haven't yet tried changing the
-        //   versions of the jenkins-test-harness et al that we're using.
+        // org.jenkinsci.plugins.workflow.job.WorkflowJob is missing its descriptor" error
+        // 
+        // You can use one instance of one type, but you can't start a new Spock spec
+        // with a new JenkinsRule using another instance of another type or you will get 
+        // this error -- At the end of the day, I want to use the resolveTestPluginsCustom 
+        // gradle task in build.gradle to test some of the Jenkins job DSL with a different
+        // set of plugins of different specs... So if there is another way to do this, 
+        // I am all ears... I haven't yet tried changing the versions of the 
+        // jenkins-test-harness that we're using.
         return CustomPluginManager.INSTANCE
     }
 }
